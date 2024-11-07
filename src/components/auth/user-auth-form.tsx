5e7086b72c88882fe4b3114a2089eb5fc23f8fa3
Lukiano99 <80914501+Lukiano99@users.noreply.github.com>
2024-11-07 14:49:53 +0100

    fix: height fixed

--- src/components/auth/user-auth-form.tsx
+++ src/components/auth/user-auth-form.tsx
@@ -30,7 +30,7 @@ export function UserAuthForm({ mode, ...props }: UserAuthFormProps) {
   }
 
   return (
-    <div className="flex h-screen w-full items-center justify-center px-4">
+    <div className="flex w-full items-center justify-center px-4">
       <Card className="mx-auto max-w-sm">
         <CardHeader>
           <CardTitle className="text-2xl text-left">
