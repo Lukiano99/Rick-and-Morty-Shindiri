5fe8eece843ad395aaadee151f9456dc4e7571bc
Lukiano99 <80914501+Lukiano99@users.noreply.github.com>
2024-11-07 12:47:06 +0100

    feat: Axios + React Query & env added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import "./App.css";
+// import "./App.css";
 import { Button } from "./components/ui/button";
 
 function App() {
+  const test = import.meta.env.VITE_FIREBASE_MEASUREMENT_ID;
   return (
     <>
-      <Button>Click me</Button>
+      <Button>Shindiri Click</Button>
+      <h2>{test}</h2>
     </>
   );
 }
